3ef2346b7512ca36f462743f51bdaddf8c68bde7
pung8146 <pung8146@naver.com>
2025-07-10 11:20:02 +0900

    오류 처리 컴포넌트 삭제: 불필요한 error.tsx 파일 제거

---
+++ src/app/(with-searchbar)/search/error.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { useRouter } from "next/router";
+import { startTransition } from "react";
+
+export default function Error({ error }: { error: Error; reset: () => void }) {
+  const router = useRouter();
+  return (
+    <div>
+      <h3>SearchError</h3>
+      <p>{error.message}</p>
+      <button
+        onClick={() => {
+          startTransition(() => {
+            router.refresh();
+            reset();
+          });
+        }}
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
