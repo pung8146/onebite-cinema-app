471d1696b3a92354c1e4633fc807769c66fb0fce
pung8146 <pung8146@naver.com>
2025-07-10 11:33:54 +0900

    로딩 상태 표시 개선: Suspense 컴포넌트의 fallback을 MovieListSkeleton으로 변경하여 영화 목록 로딩 시 사용자 경험 향상

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -3,6 +3,7 @@ import { delay } from "@/util/delay";
 import { MovieData } from "@/types";
 import style from "./page.module.css";
 import { Suspense } from "react";
+import MovieListSkeleton from "@/components/skeleton/movie-list-skeleton";
 
 async function SearchResult({ q }: { q: string }) {
   await delay(1500);
@@ -32,7 +33,10 @@ export default function SearchBar({
   }>;
 }) {
   return (
-    <Suspense key={searchParams.q} fallback={<div>Loading...</div>}>
+    <Suspense
+      key={searchParams.q}
+      fallback={<MovieListSkeleton count={3} gridColumns={3} />}
+    >
       <SearchResult q={searchParams.q || ""} />
     </Suspense>
   );
