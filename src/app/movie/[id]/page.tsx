72b73ba3cf41601ad6bf820b4be8c571c6b0afde
pung8146 <pung8146@naver.com>
2025-07-10 15:21:58 +0900

    영화 상세 페이지 구조 개선 및 리뷰 작성 기능 추가: MovieDetail 컴포넌트로 영화 정보를 분리하고, 리뷰 작성 폼을 추가하여 사용자 인터랙션 향상

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -4,24 +4,7 @@ import { MovieData } from "@/types";
 
 export const dynamicParams = false;
 
-export async function generateStaticParams() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {
-    cache: "no-cache",
-  });
-
-  const movies: MovieData[] = await res.json();
-
-  return movies.map((movie) => ({
-    id: movie.id.toString(),
-  }));
-}
-
-export default async function Page({
-  params,
-}: {
-  params: Promise<{ id: string }>;
-}) {
-  const { id: movieId } = await params;
+async function MovieDetail({ movieId }: { movieId: string }) {
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/${movieId}
     `,
@@ -36,7 +19,7 @@ export default async function Page({
   const movie: MovieData = await response.json();
 
   const {
-    id,
+    // id,
     title,
     subTitle,
     company,
@@ -48,7 +31,7 @@ export default async function Page({
   } = movie;
 
   return (
-    <div key={id} className={style.container}>
+    <section>
       <div
         className={style.cover_img_container}
         style={{ backgroundImage: `url('${posterImgUrl}')` }}
@@ -68,6 +51,45 @@ export default async function Page({
           <div className={style.description}>{description}</div>
         </div>
       </div>
+    </section>
+  );
+}
+
+export async function generateStaticParams() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`, {
+    cache: "no-cache",
+  });
+
+  const movies: MovieData[] = await res.json();
+
+  return movies.map((movie) => ({
+    id: movie.id.toString(),
+  }));
+}
+
+function ReviewEditor() {
+  async function createReviewAction(formData: FormData) {
+    "use server";
+    const content = formData.get("content")?.toString();
+    const author = formData.get("author")?.toString();
+    console.log(content, author);
+  }
+  return (
+    <section>
+      <form action={createReviewAction}>
+        <input name="content" placeholder="리뷰 내용" />
+        <input name="author" placeholder="작성자" />
+        <button type="submit">작성하기</button>
+      </form>
+    </section>
+  );
+}
+
+export default async function Page({ params }: { params: { id: string } }) {
+  return (
+    <div className={style.container}>
+      <MovieDetail movieId={params.id} />
+      <ReviewEditor />
     </div>
   );
 }
